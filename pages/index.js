44ae2760238267b4ee29e505a97664bb8f3c0ca2
Roger Souza <roger.souza@owinteractive.com>
2021-09-19 08:44:32 -0300

    Listando ETH e BIT como exemplo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 
-function Home({ data }) {
+function Home({ bitcoin, ethereum }) {
   return (
     <div className="container">
       <Head>
@@ -10,11 +10,32 @@ function Home({ data }) {
 
       <main>
         <h1 className="title">
-          <p>{data.sha}</p>
-          <p>{data.commit.author.name}</p>
-          <p>{data.commit.author.email}</p>
-          <p>{data.commit.author.date}</p>
+          <p>LAOC</p>
         </h1>
+        <div className="grid">
+          <a
+            href="https://coinmarketcap.com/currencies/bitcoin/"
+            className="card"
+          >
+            <h3>Bitcoin</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+            <p>{bitcoin.sha}</p>
+            <p>{bitcoin.commit.author.name}</p>
+            <p>{bitcoin.commit.author.email}</p>
+            <p>{bitcoin.commit.author.date}</p>
+          </a>
+          <a
+            href="https://coinmarketcap.com/currencies/ethereum/"
+            className="card"
+          >
+            <h3>Ethereum</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+            <p>{ethereum.sha}</p>
+            <p>{ethereum.commit.author.name}</p>
+            <p>{ethereum.commit.author.email}</p>
+            <p>{ethereum.commit.author.date}</p>
+          </a>
+        </div>
       </main>
 
       <footer>
@@ -28,6 +49,24 @@ function Home({ data }) {
       </footer>
 
       <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
         footer {
           width: 100%;
           height: 100px;
@@ -56,21 +95,27 @@ function Home({ data }) {
   );
 }
 
-// This function gets called at build time
 export async function getStaticProps() {
-  // Call an external API endpoint to get posts
-  const res = await fetch(
+  const res_bitcoin = await fetch(
     "https://api.github.com/repos/bitcoin/bitcoin/commits/master"
   );
-  const data = await res.json();
 
-  console.dir(data);
+  const bitcoin = await res_bitcoin.json();
 
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
+  console.dir(bitcoin);
+
+  const res_ethereum = await fetch(
+    "https://api.github.com/repos/ethereum/go-ethereum/commits/master"
+  );
+
+  const ethereum = await res_ethereum.json();
+
+  console.dir(ethereum);
+
   return {
     props: {
-      data,
+      bitcoin,
+      ethereum,
     },
   };
 }
